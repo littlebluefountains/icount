7ba817b74ecff4851275de72ab01ea349c2ab27c
littlebluefountains <littlebluefountains@yahoo.com>
2013-04-08 22:58:08 +0100

    Deployment Files New Updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,18 @@
 
 
-server "178.79.186.24", :web, :app, :db, primary: true
+# server "178.79.186.24", :web, :app, :db, primary: true
 
-set :application, "icount-granalturaoil"
-set :repository,  "https://github.com/littlebluefountains/icount.git"
-set :scm, "git"
+# set :application, "icount-granalturaoil"
+# set :repository,  "https://github.com/littlebluefountains/icount.git"
+# set :scm, "git"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -37,41 +37,42 @@ server "178.79.186.24", :web, :app, :db, primary: true
 set :application, "icount-granalturaoil"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote cache
+set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
 set :repository, "https://github.com/littlebluefountains/icount.git"
 set :branch, "master"
 
-default run options[:pty] = true
+default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup"	#keep only the last 5 releases
+
 namespace :deploy do
 	%w[start stop restart].each do |command|
 		desc "#{command} unicorn server"
-		task command, roles: :app, except: {no release: true} do
+		task command, roles: :app, except: {no_release: true} do
 			run "/etc/init.d/unicorn_#{application} #{command}"
 		end
 	end
 
-	task :setup config, roles: :app do
+	task :setup_config, roles: :app do
 		sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-		sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn #{application}"
+		sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
 		run "mkdir -p #{shared_path}/config"
 		put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
 		puts "Now edit the config files in #{shared_path}."
 	end
-	after "deploy:setup", "deploy:setup config"
+	after "deploy:setup", "deploy:setup_config"
 
-	task :symlink config, roles: :app do
-		run "ls -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+	task :symlink_config, roles: :app do
+		run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 	end
-	after "deploy:finalize update", "deploy:symlink config"
+	after "deploy:finalize_update", "deploy:symlink_config"
 
 	desc "Make sure local git is in sync with remote."
-	task :check revision, roles: :web do
+	task :check_revision, roles: :web do
 		unless `git rev-parse HEAD` == `git rev-parse origin/master`
 			puts "WARNING: HEAD is not the same as origin/master"
 			puts "Run `git push`to sync changes"
