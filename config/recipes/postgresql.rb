7b82a1bbdc6de16c0774aed29852e19bebe7e580
littlebluefountains <littlebluefountains@yahoo.com>
2013-04-10 10:54:37 +0100

    Updated Capistrano Recipes

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -4,13 +4,13 @@ set_default(:postgresql_password) { Capistrano::CLI.password_prompt "PostgreSQL 
 set_default(:postgresql_database) { "#{application}_production" }
 
 namespace :postgresql do
-	# desc "Install the latest stable release of PostgreSQL"
-	# task :install, roles: :db, only: {primary: true} do
-	# 	run "#{sudo} add-apt-repository ppa:pitti/postgresql"
-	# 	run "#{sudo} apt-get -y update"
-	# 	run "#{sudo} apt-get -y install postgresql libpq-dev"
-	# end 
-	# after "deploy:install", "postgresql:install"
+	desc "Install the latest stable release of PostgreSQL"
+	task :install, roles: :db, only: {primary: true} do
+		run "#{sudo} add-apt-repository ppa:pitti/postgresql"
+		run "#{sudo} apt-get -y update"
+		run "#{sudo} apt-get -y install postgresql libpq-dev"
+	end 
+	after "deploy:install", "postgresql:install"
 
 	desc "Create a database for this application."
 	task :create_database, roles: :db, only: {primary: true} do
