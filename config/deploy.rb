fdc73f185647796edcb9729b6420f1353cb2da3e
littlebluefountains <littlebluefountains@yahoo.com>
2013-04-30 04:54:51 +0100

    Comments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 #FROM RAILSCASTS
 require "bundler/capistrano"
 
-load "config/recipes/base"
-load "config/recipes/nginx"
-load "config/recipes/unicorn"
-load "config/recipes/postgresql"
-load "config/recipes/nodejs"
+# load "config/recipes/base"
+# load "config/recipes/nginx"
+# load "config/recipes/unicorn"
+# load "config/recipes/postgresql"
+# load "config/recipes/nodejs"
 load "config/recipes/rbenv"	#uncomment for fresh server install
 load "config/recipes/check"
 
