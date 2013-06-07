f733f0bf9f426a08a0aaa296239809151885cc6c
littlebluefountains <littlebluefountains@yahoo.com>
2013-06-07 11:59:10 +0100

    June 7 Changes to schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,8 +37,11 @@
 
 # account.save
 
+#get a connection
+conn = ActiveRecord::Base.connection
+
 ["admin", "fatgbem", "abeembolak", "bambol", "granalturaoil"].each do	|s|
-	execute "DROP SCHEMA IF EXISTS #{s}"
+	conn.execute "DROP SCHEMA IF EXISTS #{s}"
 
 	account = Account.create!(name: "#{s.upcase}", subdomain: "#{s}", website: "'www.#{s}.com'", email: "admin@#{s}.com")
 	account.create_schema
